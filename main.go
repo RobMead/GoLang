05f973edbcdd2ad0b4ce840ad628189bcf867676
Robert Mead <robjmead@gmail.com>
2019-03-02 10:36:14 +0000

    Update main.go

--- main.go
+++ main.go
@@ -16,17 +16,17 @@ func isItHim(firstName	string, lastName string ) string {
 	var testFirst string = "BENEDICT"
 	var testSecond string = "CUMBERBATCH"
 
-	// first condition - check that each name has at least 7 letters
-	if !(len(firstName) >= 7 && len(lastName) >= 7){
+	// First condition - check that each name has at least 7 letters.
+	if (len(firstName) < 7 || len(lastName) < 7){
 		return "It is not him"
 	} 
 
-	// second condition - first chars 'B' and 'C'
-	if (firstName[0] != 'B' || lastName[0] != 'C'){
+	// Second condition - first chars must be both 'B' and 'C'.
+	if !(firstName[0] == 'B' && lastName[0] == 'C'){
 		return "It is not him"
 	}
 	
-	// Check whether three characters of first name appear in "BENEDICT"
+	// Check whether three characters of first name appear in "BENEDICT".
 	var counter int = 0
 	for _, char := range firstName {
 		if strings.Contains(testFirst,string(char)){ 
@@ -38,7 +38,7 @@ func isItHim(firstName	string, lastName string ) string {
 	}
 	counter = 0
 
-	// Check whether five characters of last name appear in "CUMBERBATCH"
+	// Check whether five characters of last name appear in "CUMBERBATCH".
 	for _, char := range lastName {
 		if strings.Contains(testSecond,string(char)){
 			counter += 1
@@ -48,6 +48,6 @@ func isItHim(firstName	string, lastName string ) string {
 		return "It is not him"
 	}
 
-	//all tests passed
+	// All tests passed - return "it is him".
 	return "It is him"
 }
